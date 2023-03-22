b859ca0b4bc66e69d28c292a2da5586335004619
lvicencio <lvicencio@gmail.com>
2023-03-21 21:01:53 -0300

    listar

--- controllers/incident.js
+++ controllers/incident.js
@@ -1,7 +1,60 @@
+const Incident = require("../models/incident");
+
 function holaMundo(req, res){
     console.log("hola qliao weon");
 }
 
+
+async function createIncident(req, res) {
+    //console.log("creando incidencia");
+    const incident = new Incident();
+    const params = req.body;
+
+    incident.title = params.title;
+    incident.description = params.description;
+    incident.user = params.user;
+    incident.severity = params.severity;
+
+    //console.log(incident.title);
+
+    try {
+
+        const incidentStore = await incident.save();
+
+        if (!incidentStore) {
+            res.status(400).send({msg:"No se guardo el incidente"});
+        } else {
+            res.status(200).send({incident: incidentStore});
+        }
+        
+    } catch (error) {
+        res.status(500).send(error);
+        
+    }
+
+
+}
+
+
+async function getIncidents(req,res) {
+   // console.log("listado de incidencias");
+   try {
+        const incidents = await Incident.find().sort({create_at:1});
+
+        if (!incidents) {
+            res.status(400).send({msg:"Error no se pudo obtener incidencias"});
+        } else {
+            res.status(200).send(incidents);
+        }
+
+   } catch (error) {
+        res.status(500).send(error);
+   }
+}
+
+
 module.exports = {
-    holaMundo
+    holaMundo,
+    createIncident,
+    getIncidents
 }
--- routes/incident.js
+++ routes/incident.js
@@ -4,5 +4,7 @@ const IncidentController = require("../controllers/incident");
 const api = express.Router();
 
 api.get("/holamundo",IncidentController.holaMundo);
+api.post("/createIncident", IncidentController.createIncident);
+api.get("/listIncidents", IncidentController.getIncidents);
 
 module.exports = api;
