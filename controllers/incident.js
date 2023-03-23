e3b0122cee62890bfd8fa58e21a66a5fa4b9cd65
lvicencio <lvicencio@gmail.com>
2023-03-23 00:37:22 -0300

    crud

--- controllers/incident.js
+++ controllers/incident.js
@@ -1,3 +1,4 @@
+const { param } = require("../app");
 const Incident = require("../models/incident");
 
 function holaMundo(req, res){
@@ -53,8 +54,90 @@ async function getIncidents(req,res) {
 }
 
 
+async function getIncidentsBySeverity(req,res) {
+    const params = req.query;
+    const severity = params.severity;
+
+    try {
+        const incidents = await Incident.find({severity:severity}).sort({create_at:1});
+
+        if (!incidents) {
+            res.status(400).send({msg:"Error, incidencia no encontrada"});
+        } else {
+            res.status(200).send(incidents);
+        }
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+}
+
+
+async function getIncidentsByState(req,res) {
+    const params = req.query;
+    const completed = params.completed;
+
+    try {
+        const incidents = await Incident.find({completed:completed}).sort({create_at:1});
+
+        if (!incidents) {
+            res.status(400).send({msg:"Error, incidencia no encontrada"});
+        } else {
+            res.status(200).send(incidents);
+        }
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+}
+
+
+async function updateIncident(req, res) {
+    const params = req.body;
+    const idIncident = params.id;
+
+    try {
+        
+        const incident = await Incident.findByIdAndUpdate(idIncident,params);
+
+        if (!incident) {
+            res.send(400).send({msg:"No se pudo actualizar la incidencia"});
+        } else {
+            res.status(200).send({msg:"Se actualizo correctamente"});
+        }
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+}
+
+
+async function deleteIncident(req, res) {
+    const params = req.body;
+    const idIncident = params.id;
+
+    try {
+        const incident = await Incident.findByIdAndDelete(idIncident);
+        if (!incident) {
+            res.send(400).send({msg:"No se pudo Eliminar la Incidencia"});
+        } else {
+            res.status(200).send({msg:"Incidencia Eliminada"});
+        }
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
     holaMundo,
     createIncident,
-    getIncidents
+    getIncidents,
+    getIncidentsBySeverity,
+    getIncidentsByState,
+    updateIncident,
+    deleteIncident
 }
--- routes/incident.js
+++ routes/incident.js
@@ -6,5 +6,9 @@ const api = express.Router();
 api.get("/holamundo",IncidentController.holaMundo);
 api.post("/createIncident", IncidentController.createIncident);
 api.get("/listIncidents", IncidentController.getIncidents);
+api.get("/incidentsbyseverity",IncidentController.getIncidentsBySeverity);
+api.get("/incidentsbystate",IncidentController.getIncidentsByState);
+api.put("/updateincident",IncidentController.updateIncident);
+api.delete("/deleteincident", IncidentController.deleteIncident);
 
 module.exports = api;
